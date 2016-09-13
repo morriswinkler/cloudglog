ded4e28f97ab09c863254eba7a516ad7411f0508
Morris Winkler <m.winkler@laydrop.com>
2016-09-13 23:36:11 +0200

    * fixed CallDepth for V(LOG_LEVEL) functions
    * added all missing V(LOG_LEVEL) functions ( still missing trace)
    
    * documentation cleanup

--- logging.go
+++ logging.go
@@ -13,11 +13,11 @@ import (
 const CallDepth = 2
 
 var (
-	trace   *log.Logger
-	info    *log.Logger
-	warning *log.Logger
-	err     *log.Logger
-	fatal   *log.Logger
+	TRACE   *log.Logger
+	INFO    *log.Logger
+	WARNING *log.Logger
+	ERROR     *log.Logger
+	FATAL   *log.Logger
 
 	LogLevel int
 )
@@ -29,22 +29,22 @@ func initLogger(
 	errorHandle io.Writer,
 	fatalHandle io.Writer) {
 
-	trace = log.New(traceHandle,
+	TRACE = log.New(traceHandle,
 		"TRACE: ",
 		log.Ldate|log.Ltime|log.Llongfile)
 
-	info = log.New(infoHandle,
+	INFO = log.New(infoHandle,
 		"INFO: ",
 		log.Ldate|log.Ltime|log.Llongfile)
 
-	warning = log.New(warningHandle,
+	WARNING = log.New(warningHandle,
 		"WARNING: ",
 		log.Ldate|log.Ltime|log.Llongfile)
 
-	err = log.New(errorHandle,
+	ERROR = log.New(errorHandle,
 		"ERROR: ",
 		log.Ldate|log.Ltime|log.Llongfile)
-	fatal = log.New(fatalHandle,
+	FATAL = log.New(fatalHandle,
 		"Fatal: ",
 		log.Ldate|log.Ltime|log.Llongfile)
 
@@ -56,7 +56,6 @@ func init() {
 	getLogLevel := os.Getenv("LOG_LEVEL")
 	if len(getLogLevel) == 0 {
 		// loglevel is not in env, set to default
-		Info("missing LOG_LEVEL envieronment variable, falling back to default level 0")
 		LogLevel = 0
 	} else {
 		// loglevel is a string, convert it to int
@@ -75,19 +74,19 @@ func init() {
 // Info logs to the INFO log.
 // Arguments are handled in the manner of fmt.Print; a newline is appended if missing.
 func Info(args ...interface{}) {
-	info.Output(CallDepth, fmt.Sprint(args...))
+	INFO.Output(CallDepth, fmt.Sprint(args...))
 }
 
 // InfoDepth acts as Info but uses depth to determine which call frame to log.
 // InfoDepth(0, "msg") is the same as Info("msg").
 func InfoDepth(depth int, args ...interface{}) {
-	info.Output(depth, fmt.Sprint(args...))
+	INFO.Output(depth, fmt.Sprint(args...))
 }
 
 // Infoln logs to the INFO log.
 // Arguments are handled in the manner of fmt.Println; a newline is appended if missing.
 func Infoln(args ...interface{}) {
-	info.Output(CallDepth, fmt.Sprintln(args...))
+	INFO.Output(CallDepth, fmt.Sprintln(args...))
 }
 
 // Infof logs to the INFO log.
@@ -99,52 +98,52 @@ func Infof(format string, args ...interface{}) {
 	if buf.Bytes()[buf.Len()-1] != '\n' {
 		buf.WriteByte('\n')
 	}
-	info.Output(CallDepth, buf.String())
+	INFO.Output(CallDepth, buf.String())
 }
 
-// Warning logs to the Warning log.
+// Warning logs to the WARNING log.
 // Arguments are handled in the manner of fmt.Print; a newline is appended if missing.
 func Warning(args ...interface{}) {
-	warning.Output(CallDepth, fmt.Sprint(args...))
+	WARNING.Output(CallDepth, fmt.Sprint(args...))
 }
 
-// WarningDepth acts as Warning but uses depth to determine which call frame to log.
+// WarningDepth acts as WARNING but uses depth to determine which call frame to log.
 // WarningDepth(0, "msg") is the same as Warning("msg").
 func WarningDepth(depth int, args ...interface{}) {
-	warning.Output(depth, fmt.Sprint(args...))
+	WARNING.Output(depth, fmt.Sprint(args...))
 }
 
-// Warningln logs to the Warning log.
+// Warningln logs to the WARNING log.
 // Arguments are handled in the manner of fmt.Println; a newline is appended if missing.
 func Warningln(args ...interface{}) {
-	warning.Output(CallDepth, fmt.Sprintln(args...))
+	WARNING.Output(CallDepth, fmt.Sprintln(args...))
 }
 
-// Warningf logs to the Warning log.
+// Warningf logs to the WARNING log.
 // Arguments are handled in the manner of fmt.Printf; a newline is appended if missing.
 func Warningf(format string, args ...interface{}) {
-	warning.Output(CallDepth, fmt.Sprintf(format, args...))
+	WARNING.Output(CallDepth, fmt.Sprintf(format, args...))
 }
 
-// Error logs to the Error log.
+// Error logs to the ERROR log.
 // Arguments are handled in the manner of fmt.Print; a newline is appended if missing.
 func Error(args ...interface{}) {
-	err.Output(CallDepth, fmt.Sprint(args...))
+	ERROR.Output(CallDepth, fmt.Sprint(args...))
 }
 
-// ErrorDepth acts as Error but uses depth to determine which call frame to log.
+// ErrorDepth acts as ERROR but uses depth to determine which call frame to log.
 // ErrorDepth(0, "msg") is the same as Error("msg").
 func ErrorDepth(depth int, args ...interface{}) {
-	err.Output(depth, fmt.Sprint(args...))
+	ERROR.Output(depth, fmt.Sprint(args...))
 }
 
-// Errorln logs to the Error log.
+// Errorln logs to the ERROR log.
 // Arguments are handled in the manner of fmt.Println; a newline is appended if missing.
 func Errorln(args ...interface{}) {
-	err.Output(CallDepth, fmt.Sprintln(args...))
+	ERROR.Output(CallDepth, fmt.Sprintln(args...))
 }
 
-// Errorf logs to the Error log.
+// Errorf logs to the ERROR log.
 // Arguments are handled in the manner of fmt.Printf; a newline is appended if missing.
 func Errorf(format string, args ...interface{}) {
 
@@ -153,27 +152,27 @@ func Errorf(format string, args ...interface{}) {
 	if buf.Bytes()[buf.Len()-1] != '\n' {
 		buf.WriteByte('\n')
 	}
-	err.Output(CallDepth, buf.String())
+	ERROR.Output(CallDepth, buf.String())
 }
 
 // Fatal logs to the FATAL log
 // Arguments are handled in the manner of fmt.Print; a newline is appended if missing.
 func Fatal(args ...interface{}) {
-	fatal.Output(CallDepth, fmt.Sprint(args...))
+	FATAL.Output(CallDepth, fmt.Sprint(args...))
 }
 
-// FatalDepth acts as Fatal but uses depth to determine which call frame to log.
+// FatalDepth acts as FATAL but uses depth to determine which call frame to log.
 // FatalDepth(0, "msg") is the same as Fatal("msg").
 func FatalDepth(depth int, args ...interface{}) {
-	fatal.Output(depth, fmt.Sprint(args...))
+	FATAL.Output(depth, fmt.Sprint(args...))
 }
 
-// Fatalln logs to the log.
+// Fatalln logs to the FATAL log.
 func Fatalln(args ...interface{}) {
-	fatal.Output(CallDepth, fmt.Sprintln(args...))
+	FATAL.Output(CallDepth, fmt.Sprintln(args...))
 }
 
-// Fatalf logs to the log.
+// Fatalf logs to the FATAL log.
 // Arguments are handled in the manner of fmt.Printf; a newline is appended if missing.
 func Fatalf(format string, args ...interface{}) {
 
@@ -182,26 +181,26 @@ func Fatalf(format string, args ...interface{}) {
 	if buf.Bytes()[buf.Len()-1] != '\n' {
 		buf.WriteByte('\n')
 	}
-	fatal.Output(CallDepth, buf.String())
+	FATAL.Output(CallDepth, buf.String())
 }
 
 // Exit logs to the FATAL, ERROR, WARNING, and INFO logs, then calls os.Exit(1).
 // Arguments are handled in the manner of fmt.Print; a newline is appended if missing.
 func Exit(args ...interface{}) {
-	fatal.Output(CallDepth, fmt.Sprint(args...))
+	FATAL.Output(CallDepth, fmt.Sprint(args...))
 	os.Exit(1)
 }
 
 // ExitDepth acts as Exit but uses depth to determine which call frame to log.
 // ExitDepth(0, "msg") is the same as Exit("msg").
 func ExitDepth(depth int, args ...interface{}) {
-	fatal.Output(depth, fmt.Sprint(args...))
+	FATAL.Output(depth, fmt.Sprint(args...))
 	os.Exit(1)
 }
 
 // Exitln logs to the FATAL, ERROR, WARNING, and INFO logs, then calls os.Exit(1).
 func Exitln(args ...interface{}) {
-	fatal.Output(CallDepth, fmt.Sprintln(args...))
+	FATAL.Output(CallDepth, fmt.Sprintln(args...))
 	os.Exit(1)
 }
 
@@ -214,7 +213,7 @@ func Exitf(format string, args ...interface{}) {
 	if buf.Bytes()[buf.Len()-1] != '\n' {
 		buf.WriteByte('\n')
 	}
-	fatal.Output(CallDepth, buf.String())
+	FATAL.Output(CallDepth, buf.String())
 	os.Exit(1)
 }
 
@@ -246,7 +245,15 @@ func V(level int) Verbose {
 // See the documentation of V for usage.
 func (v Verbose) Info(args ...interface{}) {
 	if v {
-		Info(args...)
+		INFO.Output(CallDepth+1, fmt.Sprint(args...))
+	}
+}
+
+// InfoDepth is equivalent to the global InfoDepth function, guarded by the value of v.
+// See the documentation of V for usage.
+func (v Verbose) InfoDepth(depth int, args ...interface{}) {
+	if v {
+		INFO.Output(depth, fmt.Sprint(args...))
 	}
 }
 
@@ -254,7 +261,7 @@ func (v Verbose) Info(args ...interface{}) {
 // See the documentation of V for usage.
 func (v Verbose) Infoln(args ...interface{}) {
 	if v {
-		Infoln(args...)
+		INFO.Output(CallDepth+1, fmt.Sprintln(args...))
 	}
 }
 
@@ -262,6 +269,158 @@ func (v Verbose) Infoln(args ...interface{}) {
 // See the documentation of V for usage.
 func (v Verbose) Infof(format string, args ...interface{}) {
 	if v {
-		Infof(format, args...)
+		var buf bytes.Buffer
+		fmt.Fprintf(&buf, format, args...)
+		if buf.Bytes()[buf.Len()-1] != '\n' {
+			buf.WriteByte('\n')
+		}
+		INFO.Output(CallDepth+1, buf.String())
+	}
+}
+
+// Warning is equivalent to the global Warning function, guarded by the value of v.
+// See the documentation of V for usage.
+func (v Verbose) Warning(args ...interface{}) {
+	if v {
+		WARNING.Output(CallDepth+1, fmt.Sprint(args...))
+	}
+}
+
+// WarningDepth is equivalent to the global WarningDepth function, guarded by the value of v.
+// See the documentation of V for usage.
+func (v Verbose) WarningDepth(depth int, args ...interface{}) {
+	if v {
+		WARNING.Output(depth, fmt.Sprint(args...))
+	}
+}
+
+// Warningln is equivalent to the global Warningln function, guarded by the value of v.
+// See the documentation of V for usage.
+func (v Verbose)Warningln(args ...interface{}) {
+	if v {
+		WARNING.Output(CallDepth+1, fmt.Sprintln(args...))
+	}
+}
+
+// Warningf is equivalent to the global Warningf function, guarded by the value of v.
+// See the documentation of V for usage.
+func (v Verbose) Warningf(format string, args ...interface{}) {
+	if v {
+		WARNING.Output(CallDepth+1, fmt.Sprintf(format, args...))
+	}
+}
+
+// Error is equivalent to the global Error function, guarded by the value of v.
+// See the documentation of V for usage.
+func (v Verbose) Error(args ...interface{}) {
+	if v {
+		ERROR.Output(CallDepth+1, fmt.Sprint(args...))
+	}
+}
+
+// ErrorDepth is equivalent to the global ErrorDepth function, guarded by the value of v.
+// See the documentation of V for usage.
+func (v Verbose) ErrorDepth(depth int, args ...interface{}) {
+	if v {
+		ERROR.Output(depth, fmt.Sprint(args...))
+	}
+}
+
+// Errorln is equivalent to the global Errorln function, guarded by the value of v.
+// See the documentation of V for usage.
+func (v Verbose) Errorln(args ...interface{}) {
+	if v {
+		ERROR.Output(CallDepth+1, fmt.Sprintln(args...))
+	}
+}
+
+// Errorf is equivalent to the global Errorf function, guarded by the value of v.
+// See the documentation of V for usage.
+func (v Verbose) Errorf(format string, args ...interface{}) {
+	if v {
+		var buf bytes.Buffer
+		fmt.Fprintf(&buf, format, args...)
+		if buf.Bytes()[buf.Len() - 1] != '\n' {
+			buf.WriteByte('\n')
+		}
+		ERROR.Output(CallDepth+1, buf.String())
+	}
+}
+
+// Fatal is equivalent to the global Fatal function, guarded by the value of v.
+// See the documentation of V for usage.
+func (v Verbose) Fatal(args ...interface{}) {
+	if v {
+		FATAL.Output(CallDepth+1, fmt.Sprint(args...))
+	}
+}
+
+// FatalDepth is equivalent to the global FatalDepth function, guarded by the value of v.
+// See the documentation of V for usage.
+func (v Verbose) FatalDepth(depth int, args ...interface{}) {
+	if v {
+		FATAL.Output(depth, fmt.Sprint(args...))
+	}
+}
+
+// Fatalln is equivalent to the global Fatalln function, guarded by the value of v.
+// See the documentation of V for usage.
+func (v Verbose) Fatalln(args ...interface{}) {
+	if v {
+		FATAL.Output(CallDepth+1, fmt.Sprintln(args...))
+	}
+}
+
+// Fatalf is equivalent to the global Fatalf function, guarded by the value of v.
+// See the documentation of V for usage.
+func (v Verbose) Fatalf(format string, args ...interface{}) {
+	if v {
+		var buf bytes.Buffer
+		fmt.Fprintf(&buf, format, args...)
+		if buf.Bytes()[buf.Len() - 1] != '\n' {
+			buf.WriteByte('\n')
+		}
+		FATAL.Output(CallDepth+1, buf.String())
+	}
+}
+
+// Exit is equivalent to the global Exit function, guarded by the value of v.
+// See the documentation of V for usage.
+func (v Verbose) Exit(args ...interface{}) {
+	if v {
+		FATAL.Output(CallDepth+1, fmt.Sprint(args...))
+		os.Exit(1)
+	}
+}
+
+// c is equivalent to the global Exitln function, guarded by the value of v.
+// See the documentation of V for usage.
+func (v Verbose) ExitDepth(depth int, args ...interface{}) {
+	if v {
+		FATAL.Output(depth, fmt.Sprint(args...))
+		os.Exit(1)
+	}
+}
+
+// Exitln is equivalent to the global Exitln function, guarded by the value of v.
+// See the documentation of V for usage.
+func (v Verbose) Exitln(args ...interface{}) {
+	if v {
+		FATAL.Output(CallDepth+1, fmt.Sprintln(args...))
+		os.Exit(1)
+	}
+}
+
+// Exitf is equivalent to the global Exitf  function, guarded by the value of v.
+// See the documentation of V for usage.
+func (v Verbose) Exitf(format string, args ...interface{}) {
+	if v {
+		var buf bytes.Buffer
+		fmt.Fprintf(&buf, format, args...)
+		if buf.Bytes()[buf.Len() - 1] != '\n' {
+			buf.WriteByte('\n')
+		}
+		FATAL.Output(CallDepth+1, buf.String())
+		os.Exit(1)
 	}
 }
