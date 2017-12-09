5746b9cf5ddfac3c0df0e386fea83bf6debb5914
morriswinkler <m.winkler@open3dengineering.org>
2017-12-09 14:58:04 +0100

    added first test

---
+++ logging_test.go
@@ -0,0 +1,92 @@
+package cloudglog
+
+import (
+	"testing"
+	"bytes"
+	"bufio"
+	"github.com/stretchr/testify/assert"
+	"strings"
+	"os"
+)
+
+
+type logCase struct {
+	name string
+	logFunc func(...interface{})
+	args []interface{}
+	expected string
+}
+
+var logCases = []logCase{
+	{
+		name: "Info",
+		logFunc: Info,
+		args: []interface{}{"Info"},
+		expected: " Info\n",
+	},
+	{
+		name: "Infoln",
+		logFunc: Infoln,
+		args: []interface{}{"Infoln"},
+		expected: " Infoln\n",
+	},
+	{
+		name: "Warning",
+		logFunc: Warning,
+		args: []interface{}{"Warning"},
+		expected: " Warning\n",
+	},
+	{
+		name: "Warningln",
+		logFunc: Warningln,
+		args: []interface{}{"Warningln"},
+		expected: " Warningln\n",
+	},
+	{
+		name: "Error",
+		logFunc: Error,
+		args: []interface{}{"Error"},
+		expected: " Error\n",
+	},
+	{
+		name: "Errorln",
+		logFunc: Errorln,
+		args: []interface{}{"Errorln"},
+		expected: " Errorln\n",
+	},
+	{
+		name: "Fatal",
+		logFunc: Fatal,
+		args: []interface{}{"Fatal"},
+		expected: " Fatal\n",
+	},
+	{
+		name: "Fatalln",
+		logFunc: Fatalln,
+		args: []interface{}{"Fatalln"},
+		expected: " Fatalln\n",
+	},
+}
+
+func Test_LogFile(t *testing.T) {
+
+	var b bytes.Buffer
+	writer := bufio.NewWriter(&b)
+
+	LogFile(writer)
+
+	for _, testCase := range logCases {
+
+		testCase.logFunc(testCase.args...)
+		writer.Flush()
+
+		t.Log(string(b.Bytes()))
+
+		assert.Equal(t, testCase.expected, strings.Split(string(b.Bytes()), ":")[5], "%s wrong assertion for %s", t.Name(), testCase.name)
+
+		b.Reset()
+	}
+
+	os.Open()
+
+}
